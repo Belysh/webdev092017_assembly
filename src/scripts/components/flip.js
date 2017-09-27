023e9a6c7f07a864425ad20cfa72244870463b9d
redraccoon <red02raccoon@gmail.com>
2017-09-28 01:01:48 +0300

    parallax-scroll and flip some change

--- src/scripts/components/flip.js
+++ src/scripts/components/flip.js
@@ -1,21 +1,23 @@
 (function() {
+  var box = document.querySelector(".box-effect.effect__click");
 
-    var box = document.querySelector(".box-effect.effect__click");
+  var btnLogin = document.querySelector(".login__link");
+  var btnToMain = document.querySelector(".nav__link--flip");
 
+  var flip = function(btn) {
+    var c = box.classList;
+    c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+  };
 
+  btnLogin.addEventListener("click", function(e) {
+    e.preventDefault;
+    flip(this);
+    this.classList.add("hidden");
+  });
 
-        var btn = document.querySelectorAll(".changeBox");
-
-        btn.forEach(function(item){
-            item.addEventListener( "click", function(e) {
-                e.preventDefault();
-    
-                var c = box.classList;
-                c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
-    
-                this.classList.toggle("hidden");
-            })
-        })
-
-
+  btnToMain.addEventListener("click", function(e) {
+    e.preventDefault;
+    flip(this);
+    btnLogin.classList.remove("hidden");
+  });
 })();
