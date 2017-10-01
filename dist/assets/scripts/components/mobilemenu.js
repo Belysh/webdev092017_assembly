eda1a34779225c221b82825bc6043b14e57af90b
redraccoon <red02raccoon@gmail.com>
2017-10-01 20:29:04 +0300

    preloader_

--- dist/assets/scripts/components/mobilemenu.js
+++
@@ -1,8 +0,0 @@
-$(".trigger-mobilemenu").on("click", function() {
-    $(".blog").toggleClass("mobilemenu");
-});
-
-$(".article-list__item").on("click", function() {
-    $(".blog").toggleClass("mobilemenu");
-});
-
