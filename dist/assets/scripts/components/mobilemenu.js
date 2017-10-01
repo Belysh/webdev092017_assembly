d8e8f17d6660af22da4f0672899498cf6b6328ba
redraccoon <red02raccoon@gmail.com>
2017-10-01 20:35:32 +0300

    _1

---
+++ dist/assets/scripts/components/mobilemenu.js
@@ -0,0 +1,8 @@
+$(".trigger-mobilemenu").on("click", function() {
+    $(".blog").toggleClass("mobilemenu");
+});
+
+$(".article-list__item").on("click", function() {
+    $(".blog").toggleClass("mobilemenu");
+});
+
